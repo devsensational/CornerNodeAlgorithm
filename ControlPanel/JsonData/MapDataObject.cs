c674eed11b4395bc8c255e3dffcc219ff460b2eb
kwon-mingoo-3596 <kwonalsrn@naver.com>
2022-05-16 20:17:09 +0900

    GUI

---
+++ ControlPanel/JsonData/MapDataObject.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace test150x
+{
+    public class MapDataObject
+    {
+        public int width;
+        public int height;
+
+
+        //public List<Tuple<int, int>> mapDataArr = new List<Tuple<int, int>>();
+        public List<byte> mapDataArr = new List<byte>();
+
+    }
+}
